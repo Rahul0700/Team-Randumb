8a87e58e70744ce5d437345f5dcf9955d7a3575c
Rahul0700 <antorahul070@gmail.com>
2021-01-30 15:14:56 +0530

    Internal links addition

--- general/views.py
+++ general/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from accounts.models import GroupMember,Group
 # Create your views here.
 def Home(request):
-    return render(request,'home.html')
+    member_groups = []
+    admin_groups = []
+    if request.user.is_authenticated:
+        member_groups = GroupMember.objects.filter(user=request.user)
+        admin_groups = Group.objects.filter(admin=request.user.username)
+    return render(request,'home.html',{'member_groups':member_groups,
+                                       'admin_groups':admin_groups})
