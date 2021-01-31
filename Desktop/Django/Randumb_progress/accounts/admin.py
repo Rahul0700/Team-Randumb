0b9121d242c47d3c09add567347d17658fbbf049
unknown <thejineash@gmail.com>
2021-01-31 09:54:56 +0530

    Getting rid of desktop folder

--- Desktop/Django/Randumb_progress/accounts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Group,GroupMember,QuestionResponse
-# Register your models here.
-admin.site.register(Group)
-admin.site.register(GroupMember)
-admin.site.register(QuestionResponse)
--- Desktop/Django/Randumb_progress/general/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
-app_name = 'general'
-
-urlpatterns = [
-    path("model",views.predict,name="model_predict"),
-]
